d44db732187944e3e4502fea53254f95a0a5440b
omatheusmesmo <matheus.6148@gmail.com>
2024-12-16 10:03:37 -0300

    refactor: :lock: implementar UserDetails em Usuario.java e adicionar notações @Column(nullable=false)
    - Implementar UserDetails em Usuario.java
    - Adicionar notações @Column(nullable=false) nos atributos
    - Criar atributos criadoEm e atualizadoEm

--- src/main/java/com/omatheusmesmo/Lista/de/Compras/Entity/Usuario.java
+++ src/main/java/com/omatheusmesmo/Lista/de/Compras/Entity/Usuario.java
@@ -1,30 +1,86 @@
 package com.omatheusmesmo.Lista.de.Compras.Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String nomeCompleto;
+
+    @Column(unique = true, length = 100, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String senha;
+
     private String papel;
-    private Boolean ativo = true;
+
+    @CreationTimestamp
+    @Column(updatable = false, name="created_at")
+    private Date criadoEm;
+
+    @CreationTimestamp
+    @Column(name="updated_at")
+    private Date atualizadoEm;
+
+    //private Boolean ativo = true;
 
     public Usuario(String email, String senha, String papel) {
         this.email = email;
         this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
