1fe7370f868e17757cc544890498bdbfdcd45910
omatheusmesmo <matheus.6148@gmail.com>
2024-12-17 08:23:33 -0300

    feat: :lock: Implementação ApplicationConfiguration
    - Validar se usuário existe no banco
    - Configurar autenticação de usuário com UserDetailsService
    - Definir codificador de senha (BCryptPasswordEncoder)
    - Configurar AuthenticationManager

---
+++ src/main/java/com/omatheusmesmo/Lista/de/Compras/configs/ApplicationConfiguration.java
@@ -0,0 +1,49 @@
+package com.omatheusmesmo.Lista.de.Compras.configs;
+
+import com.omatheusmesmo.Lista.de.Compras.repository.UsuarioRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+    private final UsuarioRepository usuarioRepository;
+
+    public ApplicationConfiguration(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return username -> usuarioRepository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("Usuario não encontrado"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+}
