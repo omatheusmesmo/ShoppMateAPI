a5ad9f5ab7eb97657095f9a9d4539436110d6135
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-03-16 08:39:34 -0300

    feat: Add Lombok annotations to Category entity and implement findByName method in CategoryRepository

--- src/main/java/com/omatheusmesmo/shoppinglist/entity/Category.java
+++ src/main/java/com/omatheusmesmo/shoppinglist/entity/Category.java
@@ -1,11 +1,15 @@
 package com.omatheusmesmo.shoppinglist.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="categories")
+@Getter
+@Setter
 public class Category {
 
     @Id
