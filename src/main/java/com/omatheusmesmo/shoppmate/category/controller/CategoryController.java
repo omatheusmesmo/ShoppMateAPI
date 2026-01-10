25b52fc674e7f2223631450dd1c8c4a3e9537ed4
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-10 15:08:13 -0300

    feat: update CategoryController to use DTOs for improved data handling and response formatting

--- src/main/java/com/omatheusmesmo/shoppmate/category/controller/CategoryController.java
+++ src/main/java/com/omatheusmesmo/shoppmate/category/controller/CategoryController.java
@@ -1,16 +1,20 @@
 package com.omatheusmesmo.shoppmate.category.controller;
 
+import com.omatheusmesmo.shoppmate.category.dto.CategoryRequestDTO;
+import com.omatheusmesmo.shoppmate.category.dto.CategoryResponseDTO;
 import com.omatheusmesmo.shoppmate.category.entity.Category;
+import com.omatheusmesmo.shoppmate.category.mapper.CategoryMapper;
 import com.omatheusmesmo.shoppmate.category.service.CategoryService;
-import com.omatheusmesmo.shoppmate.utils.HttpResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -18,52 +22,51 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
     @Operation(summary = "Return all categories")
     @GetMapping
-    public ResponseEntity<List<Category>> getAllCategorys() {
-        try {
-            List<Category> categories = categoryService.findAll();
-            return HttpResponseUtil.ok(categories);
-        } catch (Exception e) {
-            return HttpResponseUtil.internalServerError();
-        }
+    public ResponseEntity<List<CategoryResponseDTO>> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        List<CategoryResponseDTO> responseDTOS = categories.stream()
+                .map(categoryMapper::toResponseDTO)
+                .toList();
+        return ResponseEntity.ok(responseDTOS);
     }
 
     @Operation(summary = "Add a new category")
     @PostMapping
-    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-        try {
-            categoryService.saveCategory(category);
-            return HttpResponseUtil.created(category);
-        } catch (IllegalArgumentException e) {
-            return HttpResponseUtil.badRequest(category);
-        }
+    public ResponseEntity<CategoryResponseDTO> addCategory(@RequestBody @Valid CategoryRequestDTO categoryDTO) {
+
+        Category category = categoryMapper.toEntity(categoryDTO);
+        Category savedCategory = categoryService.saveCategory(category);
+        CategoryResponseDTO responseDTO = categoryMapper.toResponseDTO(savedCategory);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedCategory.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(responseDTO);
     }
 
     @Operation(summary = "Delete a category by id")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
-        try {
-            Optional<Category> category = categoryService.findCategoryById(id);
-            assert category.orElse(null) != null;
-            categoryService.removeCategory(category.orElse(null));
-            return HttpResponseUtil.noContent();
-        } catch (NoSuchElementException exception) {
-            return HttpResponseUtil.notFound();
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.removeCategory(id);
     }
 
     @Operation(summary = "Update a category")
-    @PutMapping
-    public ResponseEntity<Category> updateCategory(@RequestBody Category category) {
-        try {
-            categoryService.saveCategory(category);
-            return HttpResponseUtil.ok(category);
-        } catch (NoSuchElementException noSuchElementException) {
-            return HttpResponseUtil.notFound();
-        } catch (IllegalArgumentException illegalArgumentException) {
-            return HttpResponseUtil.badRequest(category);
-        }
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> updateCategory(@PathVariable Long id, @RequestBody @Valid CategoryRequestDTO categoryDTO) {
+        Category category = categoryService.findCategoryById(id);
+        category.setName(categoryDTO.name());
+        Category updatedCategory = categoryService.saveCategory(category);
+        CategoryResponseDTO responseDTO = categoryMapper.toResponseDTO(updatedCategory);
+
+        return  ResponseEntity.ok(responseDTO);
     }
 }
