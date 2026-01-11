9b355c76b3e1bdc40c51f6fe050403dbdddd5789
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-11 16:13:03 -0300

    feat: enhance ListItemRepository with methods to filter soft-deleted items

--- src/main/java/com/omatheusmesmo/shoppmate/list/repository/ListItemRepository.java
+++ src/main/java/com/omatheusmesmo/shoppmate/list/repository/ListItemRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ListItemRepository extends JpaRepository<ListItem, Long> {
 
-    List<ListItem> findByShoppListId(Long shopplistId);
+    List<ListItem> findByShoppListId(Long shoppListId);
+
+    List<ListItem> findByShoppListIdAndDeletedFalse(Long shoppListId);
+
+    Optional<ListItem> findByIdAndDeletedFalse(Long id);
 
 }
