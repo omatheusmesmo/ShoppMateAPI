0c1aef701e538eff1c9296663c16ae0a42b84780
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-16 18:38:17 -0300

    feat: add method to ListPermissionRepository for retrieving active permissions by shopping list ID

--- src/main/java/com/omatheusmesmo/shoppmate/list/repository/ListPermissionRepository.java
+++ src/main/java/com/omatheusmesmo/shoppmate/list/repository/ListPermissionRepository.java
@@ -4,7 +4,11 @@ import com.omatheusmesmo.shoppmate.list.entity.ListPermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ListPermissionRepository extends JpaRepository<ListPermission, Long> {
 
+
+    List<ListPermission> findByShoppingListIdAndDeletedFalse(Long id);
 }
