1ee528b216622c0a3ce0a374a70253cbf466bf1e
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-16 17:53:08 -0300

    refactor: simplify ListPermissionMapper by removing unused repositories and updating toEntity method

--- src/main/java/com/omatheusmesmo/shoppmate/list/mapper/ListPermissionMapper.java
+++ src/main/java/com/omatheusmesmo/shoppmate/list/mapper/ListPermissionMapper.java
@@ -1,38 +1,21 @@
 package com.omatheusmesmo.shoppmate.list.mapper;
 
-import com.omatheusmesmo.shoppmate.list.entity.Permission;
 import com.omatheusmesmo.shoppmate.user.dtos.UserResponseDTO;
 import com.omatheusmesmo.shoppmate.user.entity.User;
-import com.omatheusmesmo.shoppmate.user.repository.UserRepository;
 import com.omatheusmesmo.shoppmate.list.dtos.ListPermissionRequestDTO;
 import com.omatheusmesmo.shoppmate.list.dtos.ListPermissionResponseDTO;
 import com.omatheusmesmo.shoppmate.list.entity.ListPermission;
 import com.omatheusmesmo.shoppmate.list.entity.ShoppingList;
-import com.omatheusmesmo.shoppmate.list.repository.ShoppingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.NoSuchElementException;
 
 @Component
 public class ListPermissionMapper {
 
     @Autowired
-    private ShoppingListRepository shoppingListRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
     private ListMapper listMapper;
     
-    public ListPermission toEntity(ListPermissionRequestDTO dto) {
-        ShoppingList shoppingList = shoppingListRepository.findById(dto.idList())
-                .orElseThrow(() -> new NoSuchElementException("ShoppingList not found with ID: " + dto.idList()));
-
-        User user = userRepository.findById(dto.idUser())
-                .orElseThrow(() -> new NoSuchElementException("User not found with ID: " + dto.idUser()));
-
+    public ListPermission toEntity(ListPermissionRequestDTO dto, ShoppingList shoppingList, User user) {
         ListPermission listPermission = new ListPermission();
         listPermission.setShoppingList(shoppingList);
         listPermission.setPermission(dto.permission());
@@ -53,16 +36,4 @@ public class ListPermissionMapper {
                 listPermission.getPermission()
         );
     }
-
-//    public void updateEntityFromDto(ListPermissionRequestDTO dto, ListPermission entity) {
-//         ShoppingList shoppingList = shoppingListRepository.findById(dto.listId())
-//                 .orElseThrow(() -> new NoSuchElementException("ShoppingList not found with ID: " + dto.listId()));
-//         Permission permission = userRepository.findById(dto.permissionId())
-//                 .orElseThrow(() -> new NoSuchElementException("Permission not found with ID: " + dto.permissionId()));
-//         entity.setShoppList(shoppingList);
-//         entity.setPermission(permission);
-//
-//        entity.setQuantity(dto.quantity());
-//    }
-
 }
